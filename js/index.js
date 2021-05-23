302dc9cbd6bdffbe1652dbc7845661071f44b425
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-05-23 12:41:14 +0100

    modifies javascript file

--- js/index.js
+++ js/index.js
@@ -1,8 +1,36 @@
 const toggleNavButton = document.getElementById("toggle-nav-btn");
+const newButton = document.getElementById("new-button");
 const hideableNav = document.getElementById("hideable-nav");
 const hideableSearch = document.getElementById("hideable-search");
+const newItemDropdown = document.getElementById("new-items-dropdown");
 
-toggleNavButton.addEventListener("click", () => {
-  hideableNav.classList.toggle("open-nav");
-  hideableSearch.classList.toggle("open-nav");
-});
+const getUserData = (query) => {
+  const response = {};
+
+  fetch(`https://api.github.com/graphql?query=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      response.uiDetails = data;
+    })
+    .catch((error) => {
+      response.error = error;
+    });
+
+  return response;
+};
+
+const toggleDropdown = (button, dropdown) => {
+  button.addEventListener("click", () => {
+    dropdown.classList.toggle("open-nav");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!button.contains(e.target)) {
+      dropdown.classList.remove("open-nav");
+    }
+  });
+};
+
+toggleDropdown(toggleNavButton, hideableNav);
+toggleDropdown(toggleNavButton, hideableSearch);
+toggleDropdown(newButton, newItemDropdown);
