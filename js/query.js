349dbd1ee2aba517b3e73d3f98de99644b7ec1b4
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-05-25 16:37:45 +0100

    adds method to get user data

---
+++ js/query.js
@@ -0,0 +1,31 @@
+export function getQuery(username) {
+  return `{
+  user(login: "${username}") {
+    name
+    bio
+    login
+    avatarUrl
+    status {
+      emojiHTML
+      message
+    }
+    repositories(first: 20, orderBy: {field: CREATED_AT, direction: DESC}) {
+      totalCount
+      nodes {
+        id
+        name
+        description
+        updatedAt
+        primaryLanguage {
+          name
+          color
+        }
+        licenseInfo {
+          name
+          spdxId
+        }
+      }
+    }
+  }
+}`;
+}
